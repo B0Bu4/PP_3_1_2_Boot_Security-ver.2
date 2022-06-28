61774332c48cca8b0ce24899aeed2c8fff055644
B0Bu4 <bobu41@yandex.ru>
2022-06-28 13:52:52 +0300

    fix mentor 5 (add @JsonIgnore) recursion problem

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -1,5 +1,6 @@
 package ru.kata.spring.boot_security.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,7 +25,7 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String password;
 
-
+    @JsonIgnore
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "users_and_roles",
             joinColumns = @JoinColumn(name = "user_id"),
